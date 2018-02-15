27d2d0baf73eaba3fecf45f9555f35c5a4214d5a
kensakurai <sakurai.kem@gmail.com>
2018-02-16 00:17:06 +0900

    mod jquery

--- clean_blog_frontend/src/js/index.js
+++ clean_blog_frontend/src/js/index.js
@@ -5,6 +5,7 @@ import "../css/codehilite.css";
 import "../css/font-awesome.min.css";
 import "../css/fonts.css";
 import $ from './jquery.js';
+import jQuery from './jquery.js';
 import "./bootstrap.js";
 import "./clean-blog.js";
 import "./jqBootstrapValidation.js";
