316217365abd86e5dbd78ece77eb94da49939c40
kensakurai <sakurai.kem@gmail.com>
2018-02-18 13:52:04 +0900

    mod cacheStorategy

--- clean_blog_frontend/src/js/serviceWorker.js
+++ clean_blog_frontend/src/js/serviceWorker.js
@@ -6,7 +6,7 @@ workboxSW.precache([]);
 // -------------------------------------------------------
 // runtime cache の定義
 // -------------------------------
-workboxSW.router.registerRoute(/^\/$|^\/\?utm_source.*$|^\/\?page=.+$/, workboxSW.strategies.networkFirst({
+workboxSW.router.registerRoute(/^\/$|^\/\?utm_source.*$|^\/\?page=.+$/, workboxSW.strategies.staleWhileRevalidate({
     "cacheName": "root",
     "cacheExpiration": {
         "maxAgeSeconds": 60 * 60 * 24 * 10, "maxEntries": 10
