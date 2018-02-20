71d8a1c6a50699f2a3206d68c4435d3dd119e5dd
kensakurai <sakurai.kem@gmail.com>
2018-02-20 21:44:28 +0900

    add models apis.py

---
+++ clean_blog/apis.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from rest_framework.viewsets import 
+from push_notifications.api.rest_framework import WebPushDeviceViewSet
+from push_notifications.api.rest_framework import DeviceSerializerMixin
+from push_notifications.api.rest_framework import UniqueRegistrationSerializerMixin
+from .models import WebPushCategory
+from rest_framework.serializers import ModelSerializer
+
+
+class WebPushDeviceCategoriesSerializer(UniqueRegistrationSerializerMixin, ModelSerializer):
+	class Meta(DeviceSerializerMixin.Meta):
+		model = WebPushCategory
+		fields = (
+			"id", "name", "registration_id", "active", "date_created",
+			"p256dh", "auth", "browser", "application_id", "categories",
+		)
+
+class WebPushDeviceCategoriesViewSet(WebPushDeviceViewSet):
+	queryset = WebPushCategory.objects.all()
+	serializer_class = WebPushDeviceCategoriesSerializer
--- clean_blog/models.py
+++ clean_blog/models.py
@@ -1,1 +1,18 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from push_notifications.models import WebPushDevice
+from mezzanine.blog.models import BlogCategory
+from django.utils.translation import ugettext_lazy as _
+
+class WebPushCategory(models.Model):	 
+
+	web_push_device = models.ForeignKey(WebPushDevice)
+
+	categories = models.ManyToManyField(BlogCategory,verbose_name=_("Categories"), blank=True, related_name="blogposts")
+
+	class Meta:
+		verbose_name = _("WebPush Categories")
+		verbose_name_plural = _("Blog posts")
+		ordering = ("-publish_date",)
 
