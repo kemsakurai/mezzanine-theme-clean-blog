8b71d3a4c8efa786fb7ee901851c42892ac25d6b
kensakurai <sakurai.kem@gmail.com>
2018-02-24 19:52:36 +0900

    del blog_post

--- clean_blog/apis.py
+++ clean_blog/apis.py
@@ -4,7 +4,6 @@ from push_notifications.api.rest_framework import WebPushDeviceViewSet
 from push_notifications.api.rest_framework import WebPushDeviceSerializer
 from push_notifications.api.rest_framework import DeviceSerializerMixin
 from .models import WebPushRequestInfo
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from mezzanine.blog.models import BlogPost
 from push_notifications.models import WebPushDevice
@@ -12,8 +11,6 @@ from push_notifications.models import WebPushDevice
 class WebPushRequestInfoSerializer(ModelSerializer):
 
 	web_push_device = WebPushDeviceSerializer(data = {}, required=True)
-
-	blog_post = serializers.PrimaryKeyRelatedField(many=False, queryset=BlogPost.objects.all(), read_only=False)
 
 	def create(self, validated_data):
 		web_push_device = validated_data.pop('web_push_device')
