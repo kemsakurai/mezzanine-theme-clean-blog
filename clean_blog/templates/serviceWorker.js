69a8ab5aa007d96954cd0731dd72d59b0356d6b7
kensakurai <sakurai.kem@gmail.com>
2018-03-06 00:35:16 +0900

    add includeUncontrolled

--- clean_blog/templates/serviceWorker.js
+++ clean_blog/templates/serviceWorker.js
@@ -132,7 +132,7 @@ const loadVersionBrowser = function(userAgent) {
     };
 }
 const sendMessageToAllClients = function(msg) {
-    clients.matchAll().then(clients => {
+    clients.matchAll({includeUncontrolled: true, type: 'window'}).then(function(clients) {
         clients.forEach(client => {
             sendMessageToClient(client, msg).then(m => console.log("SW Received Message: "+ m));
         })
