7b674bfe2a97f538b9aada6279a3e6637b9643df
kemsakuai <sakurai.kem@gmail.com>
2019-07-18 23:35:28 +0900

    rebuild frontend

---
+++ clean_blog/static/webpack_bundles/precache-manifest.9ed2591e0c536e1b5dc737744c295540.js
@@ -0,0 +1,69 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0869a6cd7198e7d62da2a143458211",
+    "url": "/static/webpack_bundles/1dc35d25e61d819a9c357074014867ab.ttf"
+  },
+  {
+    "revision": "afd7c0a27c42632f7216b28a473bcf4b",
+    "url": "/static/webpack_bundles/25a32416abee198dd821b0b17a198a8f.eot"
+  },
+  {
+    "revision": "b1a5b75debee62caea410a61a6639397",
+    "url": "/static/webpack_bundles/448c34a56d699c29117adc64c43affeb.woff2"
+  },
+  {
+    "revision": "a7f32003ad56c9a32e0d0df5f0d80e19",
+    "url": "/static/webpack_bundles/5e173a86206ff17b10b9d500a0a5a64c.svg"
+  },
+  {
+    "revision": "8e05a0b5b34d08685963bdbc351260f3",
+    "url": "/static/webpack_bundles/7e2cf4cef159859a7dd1794e5173dd54.svg"
+  },
+  {
+    "url": "/static/webpack_bundles/bundle-50a27ee10f40ab73c8a7.css"
+  },
+  {
+    "url": "/static/webpack_bundles/bundle-50a27ee10f40ab73c8a7.js"
+  },
+  {
+    "url": "/static/webpack_bundles/bundle-50a27ee10f40ab73c8a7.js.LICENSE"
+  },
+  {
+    "revision": "d1b68158cee4130d7eb383c2cd7ac03b",
+    "url": "/static/webpack_bundles/c8ddf1e5e5bf3682bc7bebf30f394148.woff"
+  },
+  {
+    "url": "/static/webpack_bundles/contact-50a27ee10f40ab73c8a7.js"
+  },
+  {
+    "url": "/static/webpack_bundles/contact-50a27ee10f40ab73c8a7.js.LICENSE"
+  },
+  {
+    "revision": "1fd9ee0d4f039fd9a1c2431589f9b3f2",
+    "url": "/static/webpack_bundles/e18bbf611f2a2e43afc071aa2f4e1512.ttf"
+  },
+  {
+    "revision": "cbe99f8d1c7c2666ad250c537f656e3c",
+    "url": "/static/webpack_bundles/e6cf7c6ec7c2d6f670ae9d762604cb0b.woff2"
+  },
+  {
+    "revision": "65917e6cbcbe34a1a73e46b53866c48d",
+    "url": "/static/webpack_bundles/f4769f9bdb7466be65088239c12046d1.eot"
+  },
+  {
+    "revision": "7c4f3cd55c3800d84a044a468194947d",
+    "url": "/static/webpack_bundles/fa2772327f55d8198301fdb8bcfc8158.woff"
+  },
+  {
+    "url": "/static/webpack_bundles/pjax-50a27ee10f40ab73c8a7.js"
+  },
+  {
+    "url": "/static/webpack_bundles/pjax-50a27ee10f40ab73c8a7.js.LICENSE"
+  },
+  {
+    "url": "/static/webpack_bundles/vendor-50a27ee10f40ab73c8a7.js"
+  },
+  {
+    "url": "/static/webpack_bundles/vendor-50a27ee10f40ab73c8a7.js.LICENSE"
+  }
+]);
--- clean_blog/templates/serviceWorker.js
+++ clean_blog/templates/serviceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/static/webpack_bundles/precache-manifest.32897aa4af2d292f3d34c8d63d2ae124.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/static/webpack_bundles/precache-manifest.9ed2591e0c536e1b5dc737744c295540.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = (self.__precacheManifest || []).concat([
     {
